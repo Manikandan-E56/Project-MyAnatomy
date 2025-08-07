7e97eeed509882506df1eaa7843cdd71cc8ce384
Manikandan-E56 <emanidon123@gmail.com>
2025-08-07 19:09:02 +0530

    "Second"

--- student-club-app/src/pages/Dashboard/DashBoard.jsx
+++ student-club-app/src/pages/Dashboard/DashBoard.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import Header from '../../components/Header/Header';
-
+import './DashBoard.css'; // Assuming you have a CSS file for styling
 export default function DashBoard() {
   return (
     <div>
         <Header/>
-        <h1>Welcome to the Dashboard</h1>
-        <p>This is where you can manage your clubs and events.</p>
-        {/* Add more dashboard content here */}
+        <div className="home">
+          <div className="header_text">
+            <h1>Discover Amazing Student Clubs</h1>
+          </div>
+          <img src="/dashbg.png" alt="" />
+        </div>
+        
+        
     </div>
   )
 }
